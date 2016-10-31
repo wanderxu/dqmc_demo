56e285cc8b5c81596e36b08b01e8e97aa8729c69
Xiao Yan Xu <wanderxu@gmail.com>
2016-10-31 08:45:50 +0800

    add spatial and time hybrid term in ana_confc, use more general code in chi-square.py

--- utility/ana_confc/main.f90
+++ utility/ana_confc/main.f90
@@ -1,23 +1,25 @@
 program main
 
     implicit none
-    integer :: l, ltrot, lq, nnimax, nntmax, zmax
+    integer :: l, ltrot, lq, nnimax, nntmax, nnimax_hyb, nntmax_hyb, zmax
     real(8) :: weight_track
 
     integer, dimension(:,:),   allocatable ::  nsigl_u
-    integer, dimension(:), allocatable :: b2int, jjicorr, jjtcorr
+    integer, dimension(:), allocatable :: b2int, jjcorr_i, jjcorr_t, jjcorr_it
     integer, dimension(:,:,:), allocatable :: nntable
-    integer :: iseed0, i, nn, n, nf, nt, iit, ibt, icount, nbits2int, eof, n_re
+    integer :: iseed0, i, nn, n, nf, nt, iit, ibt, icount, nbits2int, eof, n_re, nn_t, nn_i
 
     integer :: nx, ny, jx, jy, nc, ni, j, itmp, ntj
 
+    ! nnimax:  spatial
+    ! nntmax:  tempeoral
+    ! nnimax_hyb
+    ! nntmax_hyb
     ! read in parameters
     open (unit=40,file='in.para',status='unknown')
-    read(40,*) l, ltrot
+    read(40,*) l, ltrot, nnimax, nntmax, nnimax_hyb, nntmax_hyb
     close(40)
     lq = l*l
-    nnimax = 2  ! spatial
-    nntmax = 1  ! tempeoral
     zmax = 16
 #IFDEF TEST
     write(*,'(a,i6)')  ' l = ', l
@@ -30,8 +32,9 @@ program main
     ! allocate data
     allocate( nsigl_u(lq,ltrot) )
     allocate( nntable(2, zmax, nnimax) )
-    allocate( jjicorr(nnimax) )
-    allocate( jjtcorr(nntmax) )
+    allocate( jjcorr_i(nnimax) )
+    allocate( jjcorr_t(nntmax) )
+    allocate( jjcorr_it(nnimax_hyb*nntmax_hyb) )
     nbits2int = ltrot*lq/32
     if(mod(ltrot*lq,32).ne.0) nbits2int = nbits2int + 1
     allocate( b2int( nbits2int ) )
@@ -81,7 +84,7 @@ program main
 
         !!! calculate interaction energy
         ! spatial
-        jjicorr(:) = 0
+        jjcorr_i(:) = 0
         do nn = 1, nnimax
             do nt = 1, ltrot
             do nx = 1,l
@@ -96,7 +99,7 @@ program main
                     if(jy.gt.l) jy = mod(jy,l)
                     if(jy.lt.1) jy = mod(jy,l)+l
                     j = (jx-1)*l + jy
-                    jjicorr(nn) = jjicorr(nn) + nsigl_u(i,nt) * nsigl_u(j,nt)
+                    jjcorr_i(nn) = jjcorr_i(nn) + nsigl_u(i,nt) * nsigl_u(j,nt)
                 end do
             end do
             end do
@@ -104,19 +107,58 @@ program main
         end do
 
         ! temporal
-        jjtcorr(:) = 0
+        jjcorr_t(:) = 0
         do nn = 1, nntmax
             do i = 1, lq
                 do nt = 1, ltrot
                     ntj = nt+nn
                     if(ntj>ltrot) ntj = mod(ntj,ltrot)
-                    jjtcorr(nn) = jjtcorr(nn) + nsigl_u(i,nt) * nsigl_u(i,ntj)
+                    jjcorr_t(nn) = jjcorr_t(nn) + nsigl_u(i,nt) * nsigl_u(i,ntj)
                 end do
             end do
         end do
 
+        ! spatial temporal hybrid
+        jjcorr_it(:) = 0
+        do nn_i = 1, nnimax_hyb
+            do nt = 1, ltrot
+            do nx = 1,l
+            do ny = 1,l
+                i = (nx-1)*l + ny
+                do ni = 1, zmax
+                    if( nntable(1,ni,nn_i) .eq.0 .and. nntable(2,ni,nn_i) .eq. 0 ) exit
+                    jx = nx + nntable(1,ni,nn_i)
+                    jy = ny + nntable(2,ni,nn_i)
+                    if(jx.gt.l) jx = mod(jx,l)
+                    if(jx.lt.1) jx = mod(jx,l)+l
+                    if(jy.gt.l) jy = mod(jy,l)
+                    if(jy.lt.1) jy = mod(jy,l)+l
+                    j = (jx-1)*l + jy
+                    do nn_t = 1, nntmax_hyb
+
+                        nn = (nn_i-1)*nntmax_hyb + nn_t
+
+                        ntj = nt + nn_t
+                        if(ntj>ltrot) ntj = mod(ntj, ltrot)
+                        jjcorr_it(nn) = jjcorr_it(nn) + nsigl_u(i,nt) * nsigl_u(j,ntj)
+
+                        !ntj = nt - nn_t
+                        !if(ntj<1) ntj = mod(ntj, ltrot) + ltrot
+                        !jjcorr_it(nn) = jjcorr_it(nn) + nsigl_u(i,nt) * nsigl_u(j,ntj)
+                    end do
+                end do
+            end do
+            end do
+            end do
+        end do
+
         ! output
-        write(*,'(e16.8,10i20)') weight_track, jjicorr(:)/2, jjtcorr(:)
+        !write(*,'(e16.8,40i8)') weight_track, jjcorr_i(:)/2, jjcorr_t(:), jjcorr_it(:)/2
+        if( nnimax_hyb*nntmax_hyb .gt. 0 ) then
+            write(*,'(e16.8,80i8)') weight_track, jjcorr_i(:)/2, jjcorr_t(:), jjcorr_it(:)
+        else
+            write(*,'(e16.8,80i8)') weight_track, jjcorr_i(:)/2, jjcorr_t(:)
+        end if
     end do
 #IFDEF TEST
     write(*,'(a,i6)')  ' total configuration = ', nc
@@ -126,8 +168,9 @@ program main
     close(30)
 
     deallocate (nsigl_u )
-    deallocate( jjicorr )
-    deallocate( jjtcorr )
+    deallocate( jjcorr_i )
+    deallocate( jjcorr_t )
+    deallocate( jjcorr_it )
     deallocate( nntable )
 
 end program main
--- utility/trainning/chi-square.py
+++ utility/trainning/chi-square.py
@@ -31,26 +31,29 @@ def file2list(filename):
 		return returnMat
 
 ## define curvefunc for curve_fit
-def curvefunc (xv, e0, ji1, ji2, jt1 ):
-        return e0 + ji1*xv[0] + ji2*xv[1] + jt1*xv[2]
+def curvefunc (xv, *p0 ):
+        results = p0[0]
+        for i in range(len(xv)):
+            results = results + p0[i+1]*xv[i]
+        return results
 
 def chi_square ( xdata, ydata, ydata_sigma, p0 ):
         popt,pcov = opt.curve_fit(curvefunc, xdata, ydata, p0, sigma=ydata_sigma, absolute_sigma=False )
         perr = np.sqrt(np.diag(pcov))
-        rchi_sq = np.sum( ( (ydata-curvefunc(xdata,popt[0],popt[1],popt[2],popt[3]) ) / ydata_sigma )**2 )/(len(ydata)-5.0)
+        rchi_sq = np.sum( ( (ydata-curvefunc(xdata, *popt ) ) / ydata_sigma )**2 ) / len(ydata)
         return popt, perr, rchi_sq
 
 
 ## prepare data
-indat=file2list("train.dat")
+#indat=file2list("train.dat")
+indat=np.loadtxt( 'train.dat')
 inmat = np.transpose(indat)
-xdata = inmat[1:4]
+ncol=len(inmat)
+xdata = inmat[1:ncol]
 ydata = inmat[0]
 ydata_sigma =len(ydata)*[1.0]
-p0=4*[1.0]
+p0=ncol*[1.0]
 popt,perr,rchi_sq = chi_square( xdata, ydata, ydata_sigma, p0 )
-print " e0 = ", popt[0], "+/-", perr[0]
-print " j1 = ", popt[1], "+/-", perr[1]
-print " j2 = ", popt[2], "+/-", perr[2]
-print " t1 = ", popt[3], "+/-", perr[3]
+for i in range(ncol) :
+    print "p{} = {} +/- {}".format(i,popt[i],perr[i])
 print " x^2 = ", rchi_sq
